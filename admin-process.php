6ef390719756126757537c3a1b3fefdac30c0785
Andy Kan <andy0hk2012@yahoo.com.hk>
2013-10-17 15:03:07 +0800

    Create repository

---
+++ admin-process.php
@@ -0,0 +1,167 @@
+<?php
+error_reporting(-1);
+// Same as error_reporting(E_ALL);
+ini_set('error_reporting', E_ALL);
+
+include_once('lib/db.inc.php');
+
+function ierg4210_cat_fetchall() {
+	// DB manipulation
+	global $db;
+	$db = ierg4210_DB();
+	$q = $db->prepare("SELECT * FROM categories LIMIT 100;");
+	if ($q->execute())
+		return $q->fetchAll();
+}
+
+function ierg4210_cat_insert() {
+	// input validation or sanitization
+	if (!preg_match('/^[\w\-, ]+$/', $_POST['name']))
+		throw new Exception("invalid-name");
+	$name = $_POST['name'];
+	
+	// DB manipulation
+	global $db;
+	$db = ierg4210_DB();
+	$q = $db->prepare("INSERT INTO categories (name) VALUES (:name)");//catid not needed, since auto_increment
+	return $q->execute(array(':name'=>$name));
+}
+
+function ierg4210_cat_edit() {
+	// TODO: complete the rest of this function; it's now always says "successful" without doing anything
+	if (!preg_match('/^[\w\-, ]+$/', $_POST['name']))
+		throw new Exception("invalid-name");
+	$name = $_POST['name'];
+	if (!is_numeric($_POST['catid']))
+		throw new Exception("invalid-catid");
+	$catid = $_POST['catid'];
+	global $db;
+	$db = ierg4210_DB();
+	$q = $db->prepare("UPDATE categories SET name=(:name) WHERE catid=(:catid)");
+	$q->execute(array(':name'=>$name, ':catid'=>$catid));
+	return true;
+}
+
+function ierg4210_cat_delete() {
+	// input validation or sanitization
+	//$_POST['catid'] = (int) $_POST['catid'];
+	if (!is_numeric($_POST['catid']))
+		throw new Exception("invalid-catid");
+	$catid = $_POST['catid'];
+	// DB manipulation
+	global $db;
+	$db = ierg4210_DB();
+	
+	$q = $db->prepare("SELECT * FROM products WHERE catid = (:catid)");
+	if ($q->execute(array(':catid'=>$catid)))
+		if (count($q->fetchAll()) == 0){
+			$q = $db->prepare("DELETE FROM categories WHERE catid = (:catid)");
+			if ($q->execute(array(':catid'=>$catid))){
+				return true;
+			}else 
+				return false;
+		}
+	else
+		return 'Cannot delete category being linked by product(s)';
+	
+	//return $q->execute(array(':catid'=>$catid));
+}
+
+// Since this form will take file upload, we use the tranditional (simpler) rather than AJAX form submission.
+// Therefore, after handling the request (DB insert and file copy), this function then redirects back to admin.html
+function ierg4210_prod_insert() {
+	// input validation or sanitization
+
+	// DB manipulation
+	global $db;
+	$db = ierg4210_DB();
+	// TODO: complete the rest of the INSERT command
+	
+	
+	
+	
+	
+	
+	// The lastInsertId() function returns the pid (primary key) resulted by the last INSERT command
+	//$lastId = $db->lastInsertId();
+
+	// Copy the uploaded file to a folder which can be publicly accessible at incl/img/[pid].jpg
+	if ($_FILES["file"]["error"] == 0
+		&& ($_FILES["file"]["type"] == "image/jpeg" || $_FILES["file"]["type"] == "image/jpg")
+		//&& $_FILES["file"]["size"] < 5000000) {
+		&& $_FILES["file"]["size"] <= 1310720) {//1310720 bytes = 10MB
+		// Note: Take care of the permission of destination folder (hints: current user is apache)
+		//if (move_uploaded_file($_FILES["file"]["tmp_name"], "/var/www/html/incl/img/" . $lastId . ".jpg")) {
+		if (move_uploaded_file($_FILES["file"]["tmp_name"], "/var/www/html/incl/img/" . $_FILES["file"]["name"])) {
+			// redirect back to original page; you may comment it during debug
+			header('Location: ../admin.html');
+			exit();
+		}
+
+	}
+
+	// Only an invalid file will result in the execution below
+	
+	// TODO: remove the SQL record that was just inserted
+	
+	
+	// To replace the content-type header which was json and output an error message
+	header('Content-Type: text/html; charset=utf-8');
+	echo 'Invalid file detected. <br/><a href="javascript:history.back();">Back to admin panel.</a>';
+	exit();
+}
+
+// TODO: add other functions here to make the whole application complete
+function ierg4210_prod_fetchAllBy_catid() {
+	$catid = $_POST["catid"];
+	// DB manipulation
+	global $db;
+	$db = ierg4210_DB();
+	$q = $db->prepare("SELECT pid, name FROM products WHERE catid=(:cid)");
+	if ($q->execute(array(':cid' => $catid)))
+		return $q->fetchAll();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+header('Content-Type: application/json');
+
+// input validation
+if (empty($_REQUEST['action']) || !preg_match('/^\w+$/', $_REQUEST['action'])) {
+	echo json_encode(array('failed'=>'undefined'));
+	exit();
+}
+
+// The following calls the appropriate function based to the request parameter $_REQUEST['action'],
+//   (e.g. When $_REQUEST['action'] is 'cat_insert', the function ierg4210_cat_insert() is called)
+// the return values of the functions are then encoded in JSON format and used as output
+try {
+	if (($returnVal = call_user_func('ierg4210_' . $_REQUEST['action'])) === false) {
+		if ($db && $db->errorCode()) 
+			error_log(print_r($db->errorInfo(), true));
+		//echo json_encode(array('failed'=>'1'));
+		echo json_encode(array('failed'=>$db->errorCode()));
+	}
+	echo 'while(1);' . json_encode(array('success' => $returnVal));
+} catch(PDOException $e) {
+	error_log($e->getMessage());
+	echo json_encode(array('failed'=>'error-db'));
+} catch(Exception $e) {
+	echo 'while(1);' . json_encode(array('failed' => $e->getMessage()));
+}
+?>
---
+++ lib/db.inc.php
@@ -0,0 +1,23 @@
+<?php
+function ierg4210_DB() {
+	// connect to the database
+	// TODO: change the following path if needed
+	// Warning: NEVER put your db in a publicly accessible location
+	//$db = new PDO('sqlite:/var/www/db/shop.db');
+	$db = new PDO("mysql:host=localhost;dbname=ierg4210;","root","4210gp6");
+	
+	// enable foreign key support
+	$db->query('PRAGMA foreign_keys = ON;');
+
+	// FETCH_ASSOC: 
+	// Specifies that the fetch method shall return each row as an
+	// array indexed by column name as returned in the corresponding
+	// result set. If the result set contains multiple columns with
+	// the same name, PDO::FETCH_ASSOC returns only a single value
+	// per column name. 
+	$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+	//makes sure the statement and the values aren't parsed by PHP before sending it to the MySQL server (giving a possible attacker no chance to inject malicious SQL)
+	$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+	return $db;
+}
+?>
