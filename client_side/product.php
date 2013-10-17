4cae7b863e254d2ebbe94ae54469b3bbf11a728e
hzy1019 <498368245@qq.com>
2013-10-18 01:03:46 +0800

    product index.php

--- client_side/product.php
+++ client_side/product.php
@@ -18,17 +18,30 @@
 				</ul>
 			</div>
 			<!--Main Logo/Name of the Web-->
-			<h1 class="logo"><a href="index.html">Food On-line</a></h1>
+			<h1 class="logo"><a href="index.php">Food On-line</a></h1>
 		</header>
 	</div>
 	
 	<div class="navigation_container">
 		<nav>
 			<ul class="navbar"><!--Navigation Bar-->
-				<li><a href="meat.html">FRESH ARRIVAL</a></li>
-				<li><a href="meat.html">MEAT</a></li>
-				<li><a href="meat.html">VEGETABLE</a></li>
-				<li><a href="meat.html">FRUIT</a></li>
+			<?php
+					include_once('../cgi-bin/lib/db.inc.php');
+					
+					global $db;
+					$db = ierg4210_DB();
+					$q = $db->prepare("SELECT * FROM categories;");
+					$cat_array;
+					if ($q->execute())
+						$cat_array = $q->fetchAll();
+					
+					foreach ($cat_array as $cat){
+						echo '<li>';
+						echo '<a href="product.php?catid='.$cat["catid"].'">'.$cat["name"].'</a>';
+						echo '</li>';
+					}
+						
+			?>
 			</ul>
 			<!--ShoppingCart-->
 			<div class="cart"><a href="shoppingcart.html"><span>0</span> ITEM / <span>$0.00</span></a>
@@ -62,8 +75,8 @@
 		<section>
 		<!--Current Location Indicator-->
 		<ul class="sitemap">
-			<li><a href="index.html">Home</a></li>
-			<li class="active"><a href="meat.html">>Meat</a></li>
+			<li><a href="index.php">Home</a></li>
+			<li class="active"><a href="#">>Product</a></li>
 		</ul>
 		<div class="products_list"><!--procuct-list area-->
 			<ul>
@@ -83,7 +96,7 @@
 					
 					/* Underconstruction */
 					foreach ($product_array as $product){
-						echo '<li class="divide">';
+						echo '<li>';
 						echo	'<a class="product_img" href="item-details.html?catid=' . $product["catid"] .'">';
 						echo 		'<img src="' . $product["imagedir"] . '" alt="' . $product["name"] .'" />';
 						echo	'</a>';
